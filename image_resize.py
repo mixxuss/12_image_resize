737d4025abcf40b9c7b02aff8a4774d5d81fcd60
mixxuss <mixxuss@gmail.com>
2017-01-26 13:06:37 +0300

    Renamed arguments Added exception to open_image, calculate_new_size, calculate_new_image_name_and_path, main func

--- image_resize.py
+++ image_resize.py
@@ -1,22 +1,24 @@
-from __future__ import print_function
 from PIL import Image
-import os, argparse
+import os
+import argparse
 
 
 def create_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--path', help='Path to image')
-    parser.add_argument('-w', '--width', help='Width of new image', default=None)
-    parser.add_argument('-h', '--height', help='Height of new image', default=None)
-    parser.add_argument('-s', '--scale', help='Scale to resize', default=None)
-    parser.add_argument('-i', '--image', help='Image to resize path')
-    parser.add_argument('-n', '--newimage', help='Path to new image')
+    parser.add_argument('--width', help='Width of new image', default=None, type=int)
+    parser.add_argument('--height', help='Height of new image', default=None, type=int)
+    parser.add_argument('--scale', help='Scale to resize', default=None, type=int)
+    parser.add_argument('--image', help='Image to resize path')
+    parser.add_argument('--newimage', help='Path to new image', default=None)
     return parser
 
 
 def open_image(path_to_original):
-    image = Image.open(path_to_original)
-    return image
+    if os.path.exists(path_to_original):
+        image = Image.open(path_to_original)
+        return image
+    else:
+        raise FileNotFoundError('File not found, please check path to image')
 
 
 def calculate_image_ratio(image_size):
@@ -25,34 +27,57 @@ def calculate_image_ratio(image_size):
 
 
 def calc_scale(image_size, new_width, new_height):
-    if new_width:
-        scale = new_width / image_size[0]
+    if new_width is None:
+        scale = int(new_height) / image_size[1]
         return scale
-    if new_height:
-        scale = new_height / image_size[1]
+    elif new_height is None:
+        scale = int(new_width) / image_size[0]
         return scale
 
 
-def resize_image(image, scale, path_to_result):
-    new_size = (image.size[0] * scale, image.size[1] * scale)
+def calculate_new_image_size(image, scale, new_width, new_height):
+    if (scale is None and new_width is None) or (scale is None and new_height is None):
+        scale = calc_scale(image.size, new_width, new_height)
+        new_size = (int(image.size[0] * scale), int(image.size[1] * scale))
+        return new_size
+    elif new_width is not None and new_height is not None and scale is None:
+        new_size = (int(new_width), int(new_height))
+        if calculate_image_ratio(new_size) != calculate_image_ratio(image.size):
+            print('Will be a crooked image')
+        return new_size
+    elif scale is not None:
+        new_size = (int(image.size[0] * scale), int(image.size[1] * scale))
+        return new_size
+    else:
+        raise SyntaxError('Too many arguments given.')
+
+
+def calculate_new_image_name_and_path(path_to_original, path_to_new, new_size):
+    original_filepath, original_extension = os.path.splitext(path_to_original)
+    if path_to_new is None:
+        new_image_name_and_path = str(original_filepath) + '__' + str(new_size[0]) + 'x' \
+                                  + str(new_size[1]) + str(original_extension)
+        print('1=', new_image_name_and_path)
+        return new_image_name_and_path
+    new_image_name_and_path = path_to_new
+    print('2=', new_image_name_and_path)
+    return new_image_name_and_path
+
+
+def resize_image(image, new_size, new_image_name_and_path):
     out_image = image.resize(new_size)
-    out_image.save(path_to_result)
+    out_image.save(new_image_name_and_path)
 
 
 if __name__ == '__main__':
     parsed_args = create_parser()
     args = parsed_args.parse_args()
+    path_to_original = args.image
     scale = args.scale
     new_width = args.width
     new_height = args.height
+    path_to_new = args.newimage
     image = open_image(args.image)
-    if not scale:
-        scale = calc_scale(image.size, new_width, new_height)
-    resize_image(image, scale, args.newimage)
-
-'''
-    image = Image.open('123.jpg')
-    print(image.format, image.size, image.mode)
-    out_image = image.resize((250, 450))
-    out_image.save('New.jpg')
-'''
+    new_size = calculate_new_image_size(image, scale, new_width, new_height)
+    new_image_name_and_path = calculate_new_image_name_and_path(path_to_original, path_to_new, new_size)
+    resize_image(image, new_size, new_image_name_and_path)
